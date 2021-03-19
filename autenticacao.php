9e26ff033fdc633f86bc56f7172e741bb450814a
Marcos Yamashita <marcospauloyamashita@gmail.com>
2021-03-19 11:26:37 -0300

    Autenticando via ldap

--- autenticacao.php
+++ autenticacao.php
@@ -1,43 +1,68 @@
 <?php
 
-/*********************************************
-Função de validação no AD via protocolo LDAP
-como usar:
-valida_ldap("servidor", "domíniousuário", "senha");
+	
+	/*$ldap_con = ldap_connect("ldap://10.6.8.5", 389)
+        or die("Não foi possivel conectar ao servidor LDAP");
 
-*********************************************/
+	if (isset($_POST["ldapLogin"])) {
 
-function valida_ldap($srv, $usr, $pwd){
-$ldap_server = $srv;
-$auth_user = $usr;
-$auth_pass = $pwd;
+        $ldap_dn = "uid=".$_POST["usuario"].",dc=crediembrapa,dc=local";
+        $ldap_password = $_POST["senha"];
 
-// Tenta se conectar com o servidor
-if (!($connect = @ldap_connect($ldap_server))) {
-return FALSE;
-}
+        ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-//Tenta autenticar no servidor
-if (!($bind = @ldap_bind($connect, $auth_user, $auth_pass))) {
-// se não validar retorna false
-return FALSE;
-} else {
-// se validar retorna true
-return TRUE;
-}
+        //if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+        $ldapbind = @ldap_bind($ldap_con, $ldap_dn, $ldap_password);
 
-}
+        if ($ldapbind) {
 
-// EXEMPLO do uso dessa função
-$server = "samba4.crediembrapa.com.br"; //IP ou nome do servidor
-$dominio = "crediembrapa.local" //Dominio Ex: @gmail.com
-$user = "seu_usuario".$dominio;
-$pass = "sua_senha";
+            echo "Autenticado!";
+            //header('location: visualizagravacao.html');
 
-if (valida_ldap($server, $user, $pass)) {
-echo "usuário autenticado<br>";
-} else {
-echo "usuário ou senha inválida";
-}
+        } else {
+            echo "Invalid user/pass or other errors!";
+        }
+    }*/
 
+
+	
+	$ldap_con = ldap_connect("ldap://10.6.8.5:389");
+
+	ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+    if (isset($_POST["ldapLogin"])) {
+
+    $ldap_dn = "CN=usuario.servico,CN=Users,DC=crediembrapa,DC=local";
+	$ldap_password = "Credi%2018";
+
+	if(ldap_bind($ldap_con,$ldap_dn,$ldap_password))
+		echo "Authenticated";
+	else
+		echo "Invalid Credential";
+
+    }    
+	
 ?>
+
+<html>
+    <head>
+        <title>Gravações - BBB</title>
+    </head>
+    <body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>Usuário:</td>
+                <td><input type="text" name="usuario" maxlength="50"></td>
+            </tr>
+            <tr>
+                <td>Senha:</td>
+                <td><input type="password" name="senha" maxlength="50"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" name="ldapLogin" value="Entrar"></td>
+            </tr>
+        </table>
+    </form>
+    </body>
+</html>
